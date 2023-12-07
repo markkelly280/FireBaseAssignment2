7107ef9a2d5b6103de226f5de9a9a7c6ddff7ae7
markkelly280 <Kelly-M157@ulster.ac.uk>
2023-12-07 16:48:31 +0000

    Finished off Firebase code

--- app/src/main/java/com/example/firebaseassignment2/MainActivity.java
+++ app/src/main/java/com/example/firebaseassignment2/MainActivity.java
@@ -1,19 +1,103 @@
 package com.example.firebaseassignment2;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth myAuth;
+    Button SignOut;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser currentuser = myAuth.getCurrentUser();
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    OnSignInResult(result);
+                }
+            }
+
+    );
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myAuth = FirebaseAuth.getInstance();
+        createSignInIntent();
+
+        SignOut = (Button) findViewById(R.id.signout_btn);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    public void createSignInIntent(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
+
+                ;
+
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build();
+        signInLauncher.launch(signInIntent);
 
 
+    }
+    private void OnSignInResult(FirebaseAuthUIAuthenticationResult result){
+        IdpResponse response = result.getIdpResponse();
+        if(result.getResultCode()== RESULT_OK){
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(this, user.getEmail(), Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "login Failed",Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private  void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
     }
 }
