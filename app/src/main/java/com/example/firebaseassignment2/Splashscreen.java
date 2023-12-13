17257466a8e3661c2d44417532d996f56efb6175
Mont24 <127741528+Mont24@users.noreply.github.com>
2023-12-13 16:28:33 +0000

    splashscreen added

---
+++ app/src/main/java/com/example/firebaseassignment2/Splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.firebaseassignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
